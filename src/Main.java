b867d5a5685d8f58e73d967e33cebe38367baa3f
Askartius <151680098+askartius@users.noreply.github.com>
2024-03-19 20:09:03 +0300

    Practice test 4 - Q12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
+        TreeMap<String,String> map = new TreeMap<String,String>();
+        map.put("A", "1");
+        map.put("C", "2");
+        map.put("QQ", "3");
+        map.put("QW", "4");
+        map.put("R", "5");
+        map.put("S", "6");
+        map.put("T", "7");
+        map.put("QA", "8");
 
+        SortedMap<String,String> subMap = map.subMap("Q", "R");
+
+        System.out.println(subMap);
     }
 }
